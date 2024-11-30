de5d89078eda2f88e7533e36690bfbea307cf90c
Luca Botini <devbotini@gmail.com>
2024-11-30 16:39:20 -0300

    add more archives

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Wnio/test/PathTest02.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest02 {
+    public static void main(String[] args) throws IOException {
+        Path foulderPath = Paths.get("foulder");
+        if (Files.notExists(foulderPath)) {
+            Path foulderDirectory = Files.createDirectory(foulderPath);
+        }
+
+        Path subFoulderPath = Paths.get("foulder/subFoulder/subFoulder/subFoulder");
+        Path foulderDirectory = Files.createDirectories(subFoulderPath);
+        Path filePath = Paths.get(subFoulderPath.toString(), "file.txt");
+
+        if (Files.notExists(filePath)) {
+            Path filePatchCreated = Files.createFile(filePath);
+        }
+
+        Path source = filePath;
+        Path target = Paths.get("")
+
+    }
+}
